08023494d6dff0a96480663f04b7cbca59a8ac63
vkosuri <malli.kv2@gmail.com>
2024-07-14 00:07:28 +0530

    fix: fix testcase failures

--- bitop.c
+++ bitop.c
@@ -1,54 +1,42 @@
 /*
  * bitop.c
  * 
- * Copyright (c) 2024 Makarjunarao Kosuri
+ * Copyright (c) 2024 vkosuri <malli.kv2@gmail.com>
  * 
- * MIT License
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * License: MIT LICENSE, see file in root directory
  */
 
 #include "bitop.h"
 #include <stdio.h>
 #include <string.h>
 
-uint8_t set_bit(uint8_t x, uint8_t p) {
+uint8_t set_bit(uint8_t x, uint8_t p)
+{
     return (x | (1 << p));
 }
 
-uint8_t clear_bit(uint8_t x, uint8_t p) {
+uint8_t clear_bit(uint8_t x, uint8_t p)
+{
     return (x & ~(1 << p));
 }
 
-uint8_t toggle_bit(uint8_t x, uint8_t p) {
+uint8_t toggle_bit(uint8_t x, uint8_t p)
+{
     return (x ^ (1 << p));
 }
 
-uint8_t get_bit_status(uint8_t x, uint8_t p) {
+uint8_t get_bit_status(uint8_t x, uint8_t p)
+{
     return ((x >> p) & 1);
 }
 
-uint8_t is_even(uint8_t x) {
+uint8_t is_even(uint8_t x)
+{
     return !(x & 1);
 }
 
-char* format_binary(uint8_t x) {
+char* format_binary(uint8_t x)
+{
     static char res[9];
     for (int i = 7; i >= 0; i--) {
         res[7 - i] = (x & (1 << i)) ? '1' : '0';
@@ -57,7 +45,8 @@ char* format_binary(uint8_t x) {
     return res;
 }
 
-uint8_t count_set_bits(uint8_t x) {
+uint8_t count_set_bits(uint8_t x)
+{
     uint8_t count = 0;
     while (x) {
         count += x & 1;
@@ -66,11 +55,13 @@ uint8_t count_set_bits(uint8_t x) {
     return count;
 }
 
-uint8_t is_power_of_2(uint8_t x) {
+uint8_t is_power_of_2(uint8_t x)
+{
     return x && !(x & (x - 1));
 }
 
-int8_t subtract(int8_t x, int8_t y) {
+int8_t subtract(int8_t x, int8_t y)
+{
     while (y != 0) {
         int8_t borrow = (~x) & y;
         x = x ^ y;
@@ -79,7 +70,8 @@ int8_t subtract(int8_t x, int8_t y) {
     return x;
 }
 
-int8_t add(int8_t x, int8_t y) {
+int8_t add(int8_t x, int8_t y)
+{
     while (y != 0) {
         int8_t carry = x & y;
         x = x ^ y;
@@ -88,11 +80,13 @@ int8_t add(int8_t x, int8_t y) {
     return x;
 }
 
-uint8_t xor_without_operator(uint8_t x, uint8_t y) {
+uint8_t xor_without_operator(uint8_t x, uint8_t y)
+{
     return (x | y) & (~x | ~y);
 }
 
-uint8_t size_of_using_bitwise() {
+uint8_t size_of_using_bitwise()
+{
     uint8_t size = 0;
     uint8_t x = 1;
     while (x) {
@@ -102,24 +96,26 @@ uint8_t size_of_using_bitwise() {
     return size / 8;
 }
 
-uint32_t little_to_big_endian(uint32_t x) {
+uint32_t little_to_big_endian(uint32_t x)
+{
     return ((x & 0xFF000000) >> 24) |
            ((x & 0x00FF0000) >> 8) |
            ((x & 0x0000FF00) << 8) |
            ((x & 0x000000FF) << 24);
 }
 
-uint8_t multiply_by_n(uint8_t x, uint8_t n) {
+uint8_t multiply_by_n(uint8_t x, uint8_t n) 
+{
     uint8_t result = 0;
-    uint8_t multiplier = x;  // Initialize the multiplier with x
+    uint8_t multiplier = x;
 
-    // Iterate through the bits of n
+    /* Iterate through the bits of n */
     for (int i = 0; i < 8; ++i) {
         if (n & (1 << i)) {
-            // If the i-th bit of n is set, add the current multiplier to the result
+            /* If the i-th bit of n is set, add the current multiplier to the result */
             result += multiplier;
         }
-        // Update the multiplier by left-shifting it by 1
+        /* Update the multiplier by left-shifting it by 1 */
         multiplier <<= 1;
     }
 
@@ -127,22 +123,45 @@ uint8_t multiply_by_n(uint8_t x, uint8_t n) {
 }
 
 
-uint8_t is_nth_bit_on(uint32_t x, uint8_t n) {
+uint8_t is_nth_bit_on(uint32_t x, uint8_t n) 
+{
     uint8_t mask = 1 << n;
     return (x & mask) != 0;
 }
 
-uint8_t set_bits(uint8_t x, uint8_t p, uint8_t n, uint8_t y) {
+uint8_t set_bits(uint8_t x, uint8_t p, uint8_t n, uint8_t y) 
+{
     uint8_t mask = (1 << n) - 1;
     y = (y & mask) << (p - n + 1);
     mask = ~(mask << (p - n + 1));
     return (x & mask) | y;
 }
 
-void swap(uint8_t *x, uint8_t *y) {
+void swap(uint8_t *x, uint8_t *y) 
+{
     if (x != y) {
         *x ^= *y;
         *y ^= *x;
         *x ^= *y;
     }
+}
+
+uint8_t clear_lsb(uint8_t x) 
+{
+    return x & (x - 1);
+}
+
+uint8_t find_msb(uint8_t x) 
+{
+    uint8_t msb = 0;
+    while (x != 0) {
+        x >>= 1;
+        msb++;
+    }
+    return msb;
+}
+
+uint8_t swap_nibbles(uint8_t x) 
+{
+    return ((x & 0x0F) << 4) | ((x & 0xF0) >> 4);
 }
--- bitop.h
+++ bitop.h
@@ -1,27 +1,9 @@
 /*
  * bitop.h
  * 
- * Copyright (c) 2024 [Your Name]
+ * Copyright (c) 2024 vkosuri <malli.kv2@gmail.com>
  * 
- * MIT License
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * License: MIT LICENSE, see file in root directory
  */
 
 #ifndef BITOP_H
@@ -30,46 +12,46 @@
 #include <stdint.h>
 
 /**
- * @brief Sets a particular bit on an unsigned integer.
+ * @brief Sets a specific bit in a number.
  * 
- * @param x The number to modify.
- * @param p The position of the bit to set (0-7).
- * @return The modified number with the bit set.
+ * @param x The original number.
+ * @param p The position of the bit to set (0-indexed).
+ * @return The number with the specified bit set.
  */
 uint8_t set_bit(uint8_t x, uint8_t p);
 
 /**
- * @brief Clears a particular bit.
+ * @brief Clears a specific bit in a number.
  * 
- * @param x The number to modify.
- * @param p The position of the bit to clear (0-7).
- * @return The modified number with the bit cleared.
+ * @param x The original number.
+ * @param p The position of the bit to clear (0-indexed).
+ * @return The number with the specified bit cleared.
  */
 uint8_t clear_bit(uint8_t x, uint8_t p);
 
 /**
- * @brief Toggles a particular bit.
+ * @brief Toggles a specific bit in a number.
  * 
- * @param x The number to modify.
- * @param p The position of the bit to toggle (0-7).
- * @return The modified number with the bit toggled.
+ * @param x The original number.
+ * @param p The position of the bit to toggle (0-indexed).
+ * @return The number with the specified bit toggled.
  */
 uint8_t toggle_bit(uint8_t x, uint8_t p);
 
 /**
- * @brief Gets the status of a particular bit.
+ * @brief Gets the status (on/off) of a specific bit in a number.
  * 
- * @param x The number to check.
- * @param p The position of the bit to check (0-7).
- * @return 1 if the bit is set, 0 if the bit is cleared.
+ * @param x The original number.
+ * @param p The position of the bit to check (0-indexed).
+ * @return 1 if the bit is on, 0 if it is off.
  */
 uint8_t get_bit_status(uint8_t x, uint8_t p);
 
 /**
- * @brief Determines if a number is even or odd.
+ * @brief Checks if a number is even.
  * 
  * @param x The number to check.
- * @return 1 if the number is even, 0 if the number is odd.
+ * @return 1 if the number is even, 0 if it is odd.
  */
 uint8_t is_even(uint8_t x);
 
@@ -77,15 +59,15 @@ uint8_t is_even(uint8_t x);
  * @brief Formats a number as a binary string.
  * 
  * @param x The number to format.
- * @return A pointer to the binary string.
+ * @return A string representing the binary representation of x.
  */
 char* format_binary(uint8_t x);
 
 /**
- * @brief Counts the number of set bits (1s) in an integer.
+ * @brief Counts the number of set bits (1s) in a number.
  * 
- * @param x The number to check.
- * @return The number of set bits.
+ * @param x The number to count set bits in.
+ * @return The count of set bits.
  */
 uint8_t count_set_bits(uint8_t x);
 
@@ -98,7 +80,7 @@ uint8_t count_set_bits(uint8_t x);
 uint8_t is_power_of_2(uint8_t x);
 
 /**
- * @brief Subtracts two numbers without using the subtraction operator.
+ * @brief Subtracts two numbers without using the '-' operator.
  * 
  * @param x The minuend.
  * @param y The subtrahend.
@@ -107,27 +89,27 @@ uint8_t is_power_of_2(uint8_t x);
 int8_t subtract(int8_t x, int8_t y);
 
 /**
- * @brief Adds two numbers without using the addition operator.
+ * @brief Adds two numbers without using the '+' operator.
  * 
- * @param x The first addend.
- * @param y The second addend.
+ * @param x The first operand.
+ * @param y The second operand.
  * @return The sum of x and y.
  */
 int8_t add(int8_t x, int8_t y);
 
 /**
- * @brief XORs two numbers without using the XOR operator.
+ * @brief Performs XOR between two numbers without using the '^' operator.
  * 
  * @param x The first operand.
  * @param y The second operand.
- * @return The result of x ^ y.
+ * @return The result of x XOR y.
  */
 uint8_t xor_without_operator(uint8_t x, uint8_t y);
 
 /**
- * @brief Calculates the size of a type using bitwise operations.
+ * @brief Computes the size of a data type using bitwise operations.
  * 
- * @return The size of the type in bytes.
+ * @return The size of a data type (in bytes) represented by uint8_t.
  */
 uint8_t size_of_using_bitwise();
 
@@ -135,46 +117,69 @@ uint8_t size_of_using_bitwise();
  * @brief Converts a little-endian integer to a big-endian integer.
  * 
  * @param x The little-endian integer.
- * @return The big-endian integer.
+ * @return The equivalent big-endian integer.
  */
 uint32_t little_to_big_endian(uint32_t x);
 
 /**
- * @brief Multiplies a number by a positive integer using bit shifts.
+ * @brief Multiplies a number by another number using bit shifts.
  * 
  * @param x The number to multiply.
- * @param n The positive integer multiplier.
+ * @param n The multiplier (positive integer).
  * @return The result of x * n.
  */
 uint8_t multiply_by_n(uint8_t x, uint8_t n);
 
 /**
- * @brief Checks if the nth bit of a 32-bit integer is on or off.
+ * @brief Checks if a specific bit in a 32-bit integer is on.
  * 
  * @param x The number to check.
- * @param n The bit position to check (0 to 31).
- * @return 1 if the nth bit is on, 0 if it is off.
+ * @param n The position of the bit to check (0-indexed).
+ * @return 1 if the nth bit is on, 0 otherwise.
  */
 uint8_t is_nth_bit_on(uint32_t x, uint8_t n);
 
 /**
- * @brief Sets n bits of x starting from position p to the rightmost n bits of y.
+ * @brief Sets specified bits in a number from another number.
  * 
  * @param x The original number.
- * @param p The position to start setting bits.
+ * @param p The starting position (0-indexed) to set bits.
  * @param n The number of bits to set.
- * @param y The number to take bits from.
- * @return The modified number.
+ * @param y The number providing bits to set.
+ * @return The number with specified bits set from y.
  */
 uint8_t set_bits(uint8_t x, uint8_t p, uint8_t n, uint8_t y);
 
 /**
- * @brief Swaps two numbers using bitwise operations.
+ * @brief Swaps the values of two variables using bitwise XOR.
  * 
- * @param x The first number.
- * @param y The second number.
- * @return void
+ * @param x Pointer to the first variable.
+ * @param y Pointer to the second variable.
  */
 void swap(uint8_t *x, uint8_t *y);
+
+/**
+ * @brief Clears the least significant bit (LSB) of a number.
+ * 
+ * @param x The original number.
+ * @return The number with the LSB cleared.
+ */
+uint8_t clear_lsb(uint8_t x);
+
+/**
+ * @brief Finds the position of the most significant bit (MSB) in a number.
+ * 
+ * @param x The number to find MSB in.
+ * @return The position of the MSB (1-indexed).
+ */
+uint8_t find_msb(uint8_t x);
+
+/**
+ * @brief Swaps the high and low nibbles (4-bit segments) of a byte.
+ * 
+ * @param x The byte to swap nibbles in.
+ * @return The byte with swapped nibbles.
+ */
+uint8_t swap_nibbles(uint8_t x);
 
 #endif /* BITOP_H */
